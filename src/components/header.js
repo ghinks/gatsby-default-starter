51a47b650a9da9dcff74e03cf5e7b37442a1e7ae
Glenn Hinks <ghinks@yahoo.com>
2020-02-15 12:20:48 -0500

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `rgb(0,70,128)`,
       marginBottom: `1.45rem`,
     }}
   >
